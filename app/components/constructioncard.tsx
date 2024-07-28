e61bd547a3894983e7268c60c6bd60418a02e185
maxxfreund <78813326+maxxfreund@users.noreply.github.com>
2024-07-28 17:47:00 -0400

    styling changes + mobile friendly

--- app/components/constructioncard.tsx
+++ app/components/constructioncard.tsx
@@ -11,18 +11,18 @@ const ConstructionCard: React.FC<ConstructionProps> = ({ show }) => {
 
   return (
     <div className="flex backdrop-blur-lg content-center items-center justify-center z-50 w-full h-full fixed">
-      <div className="flex flex-row bg-wh rounded-xl p-10 h-fit gap-10 w-1/2">
+      <div className="flex flex-col items-center bg-wh rounded-xl p-10 h-fit gap-10 w-auto m-10">
         <img
           src={pic}
           alt={"under construction image"}
-          className={`object-contain aspect-{3/2} w-1/6 rounded-md`}
+          className={`object-contain aspect-{3/2} w-4/6 rounded-md`}
         />
         <div className="flex flex-col gap-5">
-          <h3 className="date text-xl text-dark-purple font-corbert font-bold">
-            Under Construction
-          </h3>{" "}
-          <p className="description text-xl text-dark-purple font-corbert font-bold">
-            This page is currently being worked on! Please check again later!
+          <h1 className="text-xl text-dark-purple font-corbert font-bold">
+            Under Construction!
+          </h1>{" "}
+          <p className="text-xl text-dark-purple font-corbert font-bold">
+            This page is currently being worked on. Please check again later.
           </p>
         </div>
       </div>
