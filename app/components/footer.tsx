35a1214c3166447c93b68e04524ba2ff69a908be
maxxfreund <78813326+maxxfreund@users.noreply.github.com>
2024-07-29 14:18:08 -0400

    better spacing

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import "../css/footer.css";
+import bannerLogo from "assets/icons/CGD-Dark-Banner.png";
 
 const Footer = () => {
   return (
-    <footer className="footer flex lg:flex-row flex-col py-3 px-16 lg:px-48 w-full">
-      <div className="info-wrapper-footer">
-        <div className="info">Concordia Game Development</div>
-        <a
-          href="https://goo.gl/maps/22J1SBAVLrBCwsHk7"
-          className="address"
-          target="_blank"
-        >
-          2020 Mackay St, Unit P-105, Montreal, QC H3G 2M2
-        </a>
+    <footer className="footer flex lg:flex-row flex-col py-5 px-10 lg:px-14 justify-between lg:items-center w-full">
+      <div>
+        <div className="info-wrapper-footer">
+          <div className="info">Concordia Game Development</div>
+          <a
+            href="https://goo.gl/maps/22J1SBAVLrBCwsHk7"
+            className="address"
+            target="_blank"
+          >
+            2020 Mackay St, Unit P-105, Montreal, QC H3G 2M2
+          </a>
+        </div>
+        <div className="contact-wrapper-footer">
+          <div className="contact">Contact Us</div>
+          <a href="mailto:game.development@concordia.ca" className="email">
+            game.development@concordia.ca
+          </a>
+        </div>
       </div>
-      <div className="contact-wrapper-footer">
-        <div className="contact">Contact Us</div>
-        <a href="mailto:game.development@concordia.ca" className="email">
-          game.development@concordia.ca
-        </a>
+      <div className="hidden">
+        <img src={bannerLogo} alt="banner logo" className="w-1/3 rounded-lg" />
       </div>
       <div className="social-wrapper-footer">
         <div className="social">Follow</div>
