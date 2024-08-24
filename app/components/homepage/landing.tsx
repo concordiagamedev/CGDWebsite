cfe1dd5677194883aa51fadac57afead96b62871
peaches <peizhe2001@hotmail.com>
2024-08-24 15:31:02 -0400

    add responsiveness to stats & landing components

--- app/components/homepage/landing.tsx
+++ app/components/homepage/landing.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Landing: React.FC = () => {
   return (
-    <div className="md:m-7 md:p-7 m-5 p-5 bg-amber-300 rounded-2xl md:h-[80vh] h-[35vh]">
+    <div className="md:m-7 md:p-7 m-5 p-5 bg-amber-300 rounded-2xl md:h-[80vh] sm:h-[50vh] h-[35vh]">
       <div className="flex flex-col gap-2 md:text-left text-right items-end justify-center h-full w-full ">
-        <div className="md:w-1/2 w-full md:mx-10 px-7">
-          <h1 className="gameclub sm:text-8xl text-4xl">
+        <div className="md:w-1/2 w-full md:mx-12 px-5">
+          <h1 className="gameclub xl:text-8xl lg:text-6xl md:text-5xl text-4xl">
             Concordia Game Development
           </h1>
-          <p className="sm:text-4xl font-corbert font-bold text-wh">
+          <p className="xl:text-4xl md:text-2xl text-xl font-corbert font-bold text-wh">
             Building dreams, level by level
           </p>
           <div>Button</div>
--- app/components/homepage/stats.tsx
+++ app/components/homepage/stats.tsx
@@ -4,16 +4,16 @@ import { homeStats } from "~/siteSettings/stats.json";
 const Stats: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-full">
-      <div className="flex gap-5 justify-center bg-amber-300 w-fit rounded-2xl text-center items-center p-10">
+      <div className="md:mx-10 md:p-5 mx-5 p-3 md:flex grid grid-cols-2 gap-5 justify-center bg-amber-300 md:w-fit w-full rounded-2xl text-center items-center ">
         {homeStats.map((stat) => (
           <div
             key={stat.id}
-            className="flex flex-col aspect-square justify-center items-center m-2 p-5"
+            className="flex flex-col aspect-square justify-center items-center md:m-2 md:p-5"
           >
-            <h2 className="text-5xl font-bold font-ls text-cgd-pink/100">
+            <h2 className="lg:text-5xl md:text-4xl text-2xl font-bold font-ls text-cgd-pink/100">
               {stat.stat}
             </h2>
-            <p className="text-3xl font-corbert font-bold text-dark-purple">
+            <p className="lg:text-3xl md:text-2xl text-xl font-corbert font-bold text-dark-purple">
               {stat.name}
             </p>
           </div>
