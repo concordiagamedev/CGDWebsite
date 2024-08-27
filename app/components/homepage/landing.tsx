7a78fd30469f80fa0c7477d08a70a411de9ac61d
peaches <peizhe2001@hotmail.com>
2024-08-27 12:38:58 -0400

    add background image to landing

--- app/components/homepage/landing.tsx
+++ app/components/homepage/landing.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import landing from "assets/pics/landing.png";
 
 const Landing: React.FC = () => {
   return (
-    <div className="md:m-7 md:p-7 m-5 p-5 bg-amber-300 rounded-2xl md:h-[80vh] sm:h-[50vh] h-[35vh]">
+    <div className="md:m-7  m-5 p-5 xl:h-[80vh] md:h-[55vh] h-[35vh] relative max-w-screen">
+      <img
+        src={landing}
+        alt="shows image of RDIA"
+        className="-z-10 absolute object-cover rounded-2xl h-full w-full inset-0 object-top"
+      />
       <div className="flex flex-col gap-2 md:text-left text-right items-end justify-center h-full w-full ">
         <div className="md:w-1/2 w-full md:mx-12 px-5">
-          <h1 className="gameclub xl:text-8xl lg:text-6xl md:text-5xl text-4xl">
+          <p className="gameclub xl:text-8xl lg:text-6xl md:text-5xl text-4xl text-wfull">
             Concordia Game Development
-          </h1>
-          <p className="xl:text-4xl md:text-2xl text-xl font-corbert font-bold text-wh">
+          </p>
+          <p className="xl:text-4xl md:text-2xl text-xl font-corbert font-bold text-wfull/85">
             Building dreams, level by level
           </p>
           <div>Button</div>
