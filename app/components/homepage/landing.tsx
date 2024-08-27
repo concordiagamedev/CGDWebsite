b11cbec101e1c45db2771560b6851c9e047b303e
peaches <peizhe2001@hotmail.com>
2024-08-27 12:56:18 -0400

    add border for image for accent

--- app/components/homepage/landing.tsx
+++ app/components/homepage/landing.tsx
@@ -3,11 +3,11 @@ import landing from "assets/pics/landing.png";
 
 const Landing: React.FC = () => {
   return (
-    <div className="md:m-7 md:p-7 m-5 p-5 xl:h-[90vh] md:h-[60vh] h-[40vh] relative max-w-screen">
+    <div className="md:m-7 md:p-7 m-5 p-5 xl:h-[90vh] md:h-[60vh] h-[40vh] relative max-w-screen rounded-2xl">
       <img
         src={landing}
         alt="shows image of RDIA"
-        className="-z-10 absolute object-cover rounded-2xl h-full w-full inset-0 object-top"
+        className="-z-10 absolute object-cover h-full w-full inset-0 object-top rounded-2xl border border-blue-900/70 "
       />
       <div className="flex flex-col gap-2 md:text-left text-right items-end justify-center h-full w-full ">
         <div className="md:w-1/2 w-full md:mx-12 px-5">
