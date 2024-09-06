237c085aae6c902f1d31593195e54a33bfaf1549
maxxfreund <78813326+maxxfreund@users.noreply.github.com>
2024-09-06 04:03:09 -0400

    setup for gameslots

--- app/components/homepage/gameCollection.tsx
+++ app/components/homepage/gameCollection.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { homeGames } from "~/siteSettings/homepageGames.json";
 
 const GameCollection: React.FC = () => {
   return (
@@ -6,13 +7,13 @@ const GameCollection: React.FC = () => {
       <h2 className="font-ls sm:text-6xl text-4xl md:my-14 my-6 text-center md:text-left md:mx-10 text-dark-purple uppercase">
         Game Collection
       </h2>
+      <div className="flex text-md md:text-xl font-bold font-corbert text-dark-purple">
+        Here are some member-made games during our game jam events!
+      </div>
       <div className="grid md:grid-cols-3 grid-cols-2 items-center justify-center gap-5 w-full min-h-[40vh]">
-        <div className="w-full h-full rounded-lg bg-wh/100"></div>
-        <div className="w-full h-full rounded-lg bg-wh/100"></div>
-        <div className="w-full h-full rounded-lg bg-wh/100"></div>
-        <div className="w-full h-full rounded-lg bg-wh/100"></div>
-        <div className="w-full h-full rounded-lg bg-wh/100"></div>
-        <div className="w-full h-full rounded-lg bg-wh/100"></div>
+        {homeGames.map((game) => (
+          <div className="w-full h-full rounded-lg bg-wh/100"></div>
+        ))}
       </div>
     </div>
   );
