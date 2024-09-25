6aaacb01f857be548e34f2569273b7b18ae49600
maxxfreund <78813326+maxxfreund@users.noreply.github.com>
2024-09-25 01:08:41 -0400

    styling upgrade - incomplete

--- app/components/homepage/gameCollection.tsx
+++ app/components/homepage/gameCollection.tsx
@@ -7,14 +7,25 @@ const GameCollection: React.FC = () => {
       <h2 className="font-ls sm:text-6xl text-4xl md:my-14 my-6 text-center md:text-left md:mx-10 text-dark-purple uppercase">
         Game Collection
       </h2>
-      <div className="flex text-md md:text-xl font-bold font-corbert text-dark-purple">
+      <div className="flex text-md sm:text-xl my-6 text-center md:text-left justify-center md:justify-start md:mx-10 font-bold font-corbert text-dark-purple">
         Here are some member-made games during our game jam events!
       </div>
       <div className="grid md:grid-cols-3 grid-cols-2 items-center justify-center gap-5 w-full min-h-[40vh]">
         {homeGames.map((game) => (
-          <div className="w-full h-full rounded-lg bg-wh/100">
-            <div className="m-1 p-1">
-              <img src={game.img} alt={game.alt} />
+          <div
+            className="flex w-full h-full rounded-lg bg-wh/100"
+            key={game.id}
+          >
+            <div className="m-1 p-1 ">
+              <img className="" src={game.img} alt={game.alt} />
+              <div className="justify-start flex-col">
+                <div className="text-dark-purple font-extrabold font-corbert text-md sm:text-lg md:text-xl lg:text-2xl">
+                  {game.name}
+                </div>
+                <div className="text-dark-purple font-semibold font-corbert text-xs sm:text-sm md:text-md lg:text-lg">
+                  {game.info}
+                </div>
+              </div>
             </div>
           </div>
         ))}
