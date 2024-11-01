60e041f226f47b21adfacc4024bd44c14011a9f4
jordan-vision <byjordan11@gmail.com>
2024-11-01 13:08:24 -0400

    sponsor page buttons

--- app/routes/sponsor.tsx
+++ app/routes/sponsor.tsx
@@ -6,7 +6,6 @@ import ubi from "assets/icons/ubisoft.png";
 import "app/css/sponsors.css";
 import type { MetaFunction } from "@remix-run/node";
 import ConstructionCard from "~/components/constructioncard";
-import { Icon } from "@iconify/react";
 import { Button } from "@/components/ui/button";
 import { Link } from "@remix-run/react";
 
@@ -42,50 +41,27 @@ export default function DashboardSponsor() {
             className="sponsor-img"
           />
           <div className="riseup flex flex-col lg:flex-row gap-5">
-            {/* <a
-              className="button-container"
-              target="_blank"
-              href="/uploads/CGD_sponsorship_package_2023.pdf"
-            >
-              <p className="py-4 align-middle w-full lg:w-80 text-center">
-                Click here for the detailed sponsorship package PDF
-              </p>
-            </a>
-            <a
-              className="button-container flex items-center justify-center"
-              href="mailto:game.development@concordia.ca"
-            >
-              <p className="py-4">Become a Sponsor!</p>
-            </a> */}
             <div className="flex md:gap-5 gap-3 md:justify-normal justify-end r2">
               <Button
                 asChild
-                variant={"default"}
-                className="text-wfull/85 bg-cgd-pink/85 hover:bg-cgd-pink/100 font-bold font-corbert xl:text-2xl md:text-xl text-base xl:py-8 md:pt-5 md:pb-6 pt-4 pb-5 rounded-full active:scale-95 transition-transform ease-in-out "
+                variant={"secondary"}
+                className="text-wfull/85 bg-cgd-pink/85 hover:bg-cgd-pink/100 font-bold font-corbert xl:text-2xl md:text-xl text-base xl:py-8 md:pt-5 md:pb-6 pt-4 pb-5 rounded-full active:scale-95 transition-transform ease-in-out"
               >
                 <Link
-                  to={"https://discord.gg/NfRhdRX66P"}
+                  to={"/uploads/CGD_sponsorship_package_2023.pdf"}
                   target="_blank"
                   className="flex gap-2 items-center"
                 >
-                  <Icon
-                    icon="ic:baseline-discord"
-                    className="text-wfull/85 xl:text-5xl md:text-3xl text-2xl"
-                  />
-                  Join Us
+                  Click here for the detailed sponsorship package PDF
                 </Link>
               </Button>
               <Button
                 asChild
-                variant={"secondary"}
+                variant={"default"}
                 className="text-cgd-pink/85 hover:text-cgd-pink bg-wfull/85 hover:bg-wfull font-bold font-corbert xl:text-2xl md:text-xl text-base xl:py-8 md:pt-5 md:pb-6 pt-4 pb-5 rounded-full active:scale-95 transition-transform ease-in-out"
               >
-                <Link to={"/sponsor"} className="flex gap-2 items-center">
-                  <Icon
-                    icon="solar:hand-stars-bold"
-                    className="text-cgd-pink/85 xl:text-5xl md:text-3xl text-2xl"
-                  />
-                  Sponsor Us
+                <Link to={"mailto:game.development@concordia.ca"} className="flex gap-2 items-center">
+                  Become a Sponsor!
                 </Link>
               </Button>
             </div>
